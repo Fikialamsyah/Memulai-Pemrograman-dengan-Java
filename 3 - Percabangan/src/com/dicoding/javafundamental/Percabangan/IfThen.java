8c2cd0c0237f66b1ad96987670445b745991a655
Fiki Alamsyah <fikialamsyah16@gmai.com>
2020-10-12 23:06:14 +0700

    Menambahkan folder 3 - Percabangan

---
+++ 3 - Percabangan/src/com/dicoding/javafundamental/Percabangan/IfThen.java
@@ -0,0 +1,11 @@
+package com.dicoding.javafundamental.Percabangan;
+
+public class IfThen {
+    public static void main(String[] args) {
+        boolean isOn = true;
+
+        if (isOn){
+            System.out.println("Menyalakan lampu");
+        }
+    }
+}
